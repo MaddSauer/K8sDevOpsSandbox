95132f653647a30a723163b77ce9db8d0959f14e
Madd Sauer <github@sauer.ms>
2025-04-10 16:53:37 +0200

    Update brew_update.sh

--- brew_update.sh
+++ brew_update.sh
@@ -1,4 +1,13 @@
 /usr/bin/env bash
 brew bundle --file="./Brewfile"
 
+brew tap snyk/tab
+brew install snyk
+
+brew tap stackitcloud/tap
+brew install stackit
+
+brew tap k8sgpt-ai/k8sgpt
+brew install k8sgpt
+
 echo "use bettertouchtool as a replacement to run cronjobs on macOS"
